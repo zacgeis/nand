3f4224d79af5bf2e834d3310bfb436f34e91b894
zacgeis <zacgeis@gmail.com>
2016-04-05 22:04:07 -0500

    Decrease performance scale

--- test/perfTest.js
+++ test/perfTest.js
@@ -4,14 +4,14 @@ var helper = require('./helper');
 var Circuit = require('../lib/circuit');
 var gates = require('../lib/gates');
 
-describe('performance', function() {
-  it('with 100K chained slow ORs', function() {
+describe('performs', function() {
+  it('with 50K OR gates', function() {
     var c = new Circuit();
     var w1 = c.wire();
     var w2 = w1
     var w3;
 
-    for(var i = 0; i < 100000; i++) {
+    for(var i = 0; i < 50000; i++) {
       w3 = c.wire();
       gates.or(c, w2, w2, w3);
       w2 = w3;
